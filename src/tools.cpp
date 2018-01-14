fd30f839861b2be3515c3b19826856d9419b6f51
Sanket Gujar <sanketgujar95@gmail.com>
2018-01-13 20:28:29 -0500

    Completed Jacobian matrix in Tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -22,4 +22,27 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+	//Please see equation 96 in reference pdf for Jacobian matrix..
+	MatrixXd Hj_(3,4);
+
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+
+	float a1 = ( pow( px , 2 ) + pow( py , 2 ) );
+	if ( fabs(a1) < 0.0001){
+		cout << "Division by Zero Error in Tools::CalculateJacobian ";
+		return Hj_;
+	}
+	float a2 = sqrt( a1 );
+	float a3 = a1 * a2 ;
+
+	Hj_ << px / a2 , py / a2 , 0 , 0 ,
+		   -py / a1 , px / a1 , 0 , 0 ,
+		   (py * (vx * py - vy * px )) / a3 , (px * (vy * px - vx * py ))/a3 , px / a2 , py / a2 ;
+	
+	return Hj_; 
+
 }
